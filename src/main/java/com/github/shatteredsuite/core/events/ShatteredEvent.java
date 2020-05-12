bbbed4d78264e00be48fdb2004448f36c866df41
UberPilot <uberpilot99@gmail.com>
2020-05-12 16:54:37 -0600

    Add basic custom event system.

--- src/main/java/com/github/shatteredsuite/core/events/ShatteredEvent.java
+++ src/main/java/com/github/shatteredsuite/core/events/ShatteredEvent.java
@@ -1,4 +1,32 @@
 package com.github.shatteredsuite.core.events;
 
-public class ShatteredEvent {
+import org.bukkit.event.Cancellable;
+import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
+import org.jetbrains.annotations.NotNull;
+
+public abstract class ShatteredEvent extends Event implements Cancellable {
+    protected static final HandlerList handlers = new HandlerList();
+    protected boolean cancelled = false;
+
+    @NotNull
+    public static HandlerList getHandlerList() {
+        return handlers;
+    }
+
+    @Override
+    public boolean isCancelled() {
+        return cancelled;
+    }
+
+    @Override
+    public void setCancelled(boolean cancelled) {
+        this.cancelled = cancelled;
+    }
+
+    @NotNull
+    @Override
+    public HandlerList getHandlers() {
+        return handlers;
+    }
 }
