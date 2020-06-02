746de366d35fb77efe5221b033e256238b940efb
UberPilot <uberpilot99@gmail.com>
2020-06-01 23:19:40 -0600

    Register ConfigRecipe as serializable.

--- src/main/java/com/github/shatteredsuite/core/plugin/ShatteredCore.java
+++ src/main/java/com/github/shatteredsuite/core/plugin/ShatteredCore.java
@@ -1,9 +1,17 @@
 package com.github.shatteredsuite.core.plugin;
 
 import com.github.shatteredsuite.core.ShatteredPlugin;
+import com.github.shatteredsuite.core.config.ConfigRecipe;
+import org.bukkit.configuration.serialization.ConfigurationSerialization;
 
 public class ShatteredCore extends ShatteredPlugin {
     protected int bStatsId = 7496;
+
+
+    @Override
+    protected void load() {
+        ConfigurationSerialization.registerClass(ConfigRecipe.class);
+    }
 
     @Override
     protected void postEnable() {
