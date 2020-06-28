59695667130adc18a22a070d9c9cf15095c3ec56
Hunter Henrichsen <hunter.henrichsen@gmail.com>
2020-06-27 21:13:03 -0600

    :sparkles: Add basic tabcompleter to ParameterizedBranchCommand

--- src/main/java/com/github/shatteredsuite/core/commands/ParameterizedBranchCommand.java
+++ src/main/java/com/github/shatteredsuite/core/commands/ParameterizedBranchCommand.java
@@ -4,7 +4,12 @@ import com.github.shatteredsuite.core.commands.predicates.*;
 import com.github.shatteredsuite.core.commands.responses.CancelResponse;
 import com.github.shatteredsuite.core.messages.Messageable;
 import com.github.shatteredsuite.core.util.ArrayUtil;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.Collections;
+import java.util.List;
 
 public abstract class ParameterizedBranchCommand extends WrappedCommand {
     public ParameterizedBranchCommand(Messageable instance, WrappedCommand parent, String label, String permission, String helpPath) {
@@ -18,5 +23,16 @@ public abstract class ParameterizedBranchCommand extends WrappedCommand {
         children.get(context.args[1]).execute(new CommandContext(children.get(context.args[1]), context.sender,
                 context.label + context.args[1], ArrayUtil.withoutIndex(context.args, 1),
                 context.messenger, context.cancelled, context.args[0]));
+    }
+
+    @Override
+    public List<String> onTabComplete(CommandSender sender, Command command, String alias, String[] args) {
+        WrappedCommand cmd = children.get(args[1]);
+        if(cmd == null) {
+            return Collections.emptyList();
+        }
+        else {
+            return cmd.onTabComplete(sender, command, alias, ArrayUtil.withoutIndex(args, 1));
+        }
     }
 }
