44c4359dd62a4a1924506acb70a8986ef61c12fd
Hunter Henrichsen <hunter.henrichsen@gmail.com>
2020-08-25 18:32:20 -0600

    Revert ":ant: Remove references to unfinished features."
    This reverts commit ef4615a76087bc0be0670adf9f3f1c04528f7bcf.

--- src/main/java/com/github/shatteredsuite/core/validation/Validator.java
+++ src/main/java/com/github/shatteredsuite/core/validation/Validator.java
@@ -1,8 +1,14 @@
 package com.github.shatteredsuite.core.validation;
 
+import com.github.shatteredsuite.core.conversion.Conversion;
 import org.jetbrains.annotations.NotNull;
 
-public interface Validator<T> {
+public interface Validator<T> extends Conversion<String, T> {
     @NotNull
     T validate(@NotNull String param) throws ArgumentValidationException;
+
+    @Override
+    default T convert(@NotNull String source) {
+        return validate(source);
+    }
 }
