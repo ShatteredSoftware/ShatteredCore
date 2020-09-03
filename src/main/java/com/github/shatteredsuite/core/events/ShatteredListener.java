465f0728e81c125a44e968941f22ef7acff0c803
Hunter Henrichsen <hunter.henrichsen@gmail.com>
2020-09-03 17:05:47 -0600

    :sparkles: Add new ShatteredListener.

---
+++ src/main/java/com/github/shatteredsuite/core/events/ShatteredListener.java
@@ -0,0 +1,16 @@
+package com.github.shatteredsuite.core.events;
+
+import com.github.shatteredsuite.core.ShatteredPlugin;
+import org.bukkit.event.Listener;
+
+public class ShatteredListener implements Listener {
+    protected final ShatteredPlugin plugin;
+
+    ShatteredListener(ShatteredPlugin plugin) {
+        this.plugin = plugin;
+    }
+
+    public void register() {
+        plugin.getServer().getPluginManager().registerEvents(this, plugin);
+    }
+}
