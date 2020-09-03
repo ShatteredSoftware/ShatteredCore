26b5cb7bf44fe6a96952dc2e31c6fc4c47b66b22
Hunter Henrichsen <hunter.henrichsen@gmail.com>
2020-09-03 17:12:21 -0600

    :wrench: Add correct access modifier to new extension.

--- src/main/kotlin/com/github/shatteredsuite/core/ext/FileExt.kt
+++ src/main/kotlin/com/github/shatteredsuite/core/ext/FileExt.kt
@@ -2,6 +2,6 @@ package com.github.shatteredsuite.core.ext
 
 import java.io.File
 
-private operator fun File.get(s: String): File {
+operator fun File.get(s: String): File {
     return File(this, s)
 }
