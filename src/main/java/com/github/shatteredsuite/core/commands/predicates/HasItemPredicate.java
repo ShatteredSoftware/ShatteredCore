6989170bdfc9426be0fc3169e52dc951b99807dc
Scibs <mscibilia91@gmail.com>
2020-10-09 10:07:08 +0800

    Issue #7: Change HasItemPredicate to check for a specific ItemStack using a given ItemStack.

--- src/main/java/com/github/shatteredsuite/core/commands/predicates/HasItemPredicate.java
+++ src/main/java/com/github/shatteredsuite/core/commands/predicates/HasItemPredicate.java
@@ -4,22 +4,24 @@ import com.github.shatteredsuite.core.commands.responses.PredicateResponse;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 
+import java.util.Arrays;
+
 public class HasItemPredicate extends SenderPlayerPredicate {
 
-    private final int index;
+    private final ItemStack itemStack;
 
-    public HasItemPredicate(PredicateResponse response, int index) {
+    public HasItemPredicate(PredicateResponse response, ItemStack itemStack) {
         super(response);
-        this.index = index;
-        this.name = "Player has an item.";
+        this.itemStack = itemStack;
+        this.name = "Player has the item.";
     }
 
     @Override
     public boolean test(CommandContext context) {
         boolean playerHasItem = false;
         if (super.test(context)) {
-            ItemStack itemStack = ((Player) context.sender).getInventory().getItem(index);
-            playerHasItem = itemStack != null && itemStack.getAmount() > 0;
+            ItemStack[] inventoryContents = ((Player) context.sender).getInventory().getContents();
+            playerHasItem = Arrays.asList(inventoryContents).contains(itemStack);
         }
         return playerHasItem;
     }
