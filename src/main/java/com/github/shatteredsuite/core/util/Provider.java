402ff07b896fcaf677a00b59a9d54e386ba225a3
Hunter Henrichsen <hunter.henrichsen@gmail.com>
2020-11-02 13:43:12 -0700

    :sparkles: Allow converting Suppliers to Providers.

--- src/main/java/com/github/shatteredsuite/core/util/Provider.java
+++ src/main/java/com/github/shatteredsuite/core/util/Provider.java
@@ -1,9 +1,15 @@
 package com.github.shatteredsuite.core.util;
 
+import java.util.function.Supplier;
+
 public interface Provider<T> {
     T get();
 
     static <K> Provider<K> of(K t) {
         return () -> t;
+    }
+
+    static <K> Provider<K> of(Supplier<K> supplier) {
+        return supplier::get;
     }
 }
