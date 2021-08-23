15ad015440d775185a976dd3ea8aebf0d3a9267d
Hunter Henrichsen <hunter.henrichsen@gmail.com>
2021-08-22 19:26:08 -0600

    :recycle: Update MessageProcessor to take an optional GenericDataStore.

--- src/main/kotlin/com/github/shatteredsuite/core/message/MessageProcessor.kt
+++ src/main/kotlin/com/github/shatteredsuite/core/message/MessageProcessor.kt
@@ -1,7 +1,8 @@
 package com.github.shatteredsuite.core.message
 
+import com.github.shatteredsuite.core.data.persistence.GenericDataStore
 import com.ibm.icu.util.ULocale
 
 interface MessageProcessor {
-    fun process(message: String, locale: ULocale): String
+    fun process(message: String, data: GenericDataStore? = null, locale: ULocale = ULocale.US): String
 }
--- src/main/kotlin/com/github/shatteredsuite/core/message/processor/LangProcessor.kt
+++ src/main/kotlin/com/github/shatteredsuite/core/message/processor/LangProcessor.kt
@@ -1,11 +1,12 @@
 package com.github.shatteredsuite.core.message.processor
 
+import com.github.shatteredsuite.core.data.persistence.GenericDataStore
 import com.github.shatteredsuite.core.message.MessageProcessor
 import com.github.shatteredsuite.core.message.lang.MessageSet
 import com.ibm.icu.util.ULocale
 
 class LangProcessor(var messageSet: MessageSet): MessageProcessor {
-    override fun process(message: String, locale: ULocale): String {
-        return messageSet.get(message, locale = locale) ?: throw Exception("Missing message $message.")
+    override fun process(message: String, data: GenericDataStore?, locale: ULocale): String {
+        return messageSet.get(message, data, locale) ?: throw Exception("Missing message $message.")
     }
 }
